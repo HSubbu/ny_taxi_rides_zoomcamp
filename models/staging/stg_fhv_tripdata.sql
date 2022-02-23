405d610d1e24828bc1be57c11ad878b9dd197c3d
Subramanian Hariharan <subramanianhari91@gmail.com>
2022-02-23 17:52:40 +0000

    23feb1250

--- models/staging/stg_fhv_tripdata.sql
+++ models/staging/stg_fhv_tripdata.sql
@@ -1,11 +1,11 @@
 {{ config(materialized='view') }}
 
-with fhv_tripdata as 
+with tripdata as 
 (
   select *,
-    row_number() over(partition by vendorid, lpep_pickup_datetime) as rn
+    row_number() over(partition by dispatching_base_num, pickup_datetime) as rn
   from {{ source('staging','fhv_tripdata') }}
-  
+  where dispatching_base_num is not null 
 )
 select
     -- identifiers
@@ -17,7 +17,7 @@ select
     cast(pickup_datetime as timestamp) as pickup_datetime,
     cast(dropoff_datetime as timestamp) as dropoff_datetime,
     
-from fhv_tripdata
+from tripdata
 
 
 -- dbt build --m <model.sql> --var 'is_test_run: false'
